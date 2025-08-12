3fa03c8daf6592463a557b6a2d3d51913ad9fdff
Shabb007 <sebkay2002@hotmail.com>
2025-08-13 00:07:41 +0300

    last

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,14 @@
-import { ColorRing } from "react-loader-spinner";
+import { ClipLoader } from "react-spinners";
 
 export const Loader = () => {
   return (
     <div
       style={{ display: "flex", justifyContent: "center", marginTop: "300px" }}
     >
-      <ColorRing
-        visible={true}
-        height="120"
-        width="120"
-        ariaLabel="color-ring-loading"
-        wrapperStyle={{}}
-        wrapperClass="color-ring-wrapper"
-        colors={["#F4C550", "#F2C0BD", "#121417", "#F4C8BA", "#9FB7CE"]}
+      <ClipLoader
+        color="#F4C550"
+        size={60}
+        aria-label="Loading Spinner"
       />
     </div>
   );
